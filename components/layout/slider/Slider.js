b8674c3df320ccc2d0dd7c2a02c723b889be694d
KhoiNg <minhkhoi260199@gmail.com>
2022-02-15 20:32:15 +0700

    Update
    scroll button + fixBug

--- components/layout/slider/Slider.js
+++ components/layout/slider/Slider.js
@@ -1,7 +1,7 @@
 import { ChevronDownIcon } from '@chakra-ui/icons';
 import { Box, Button, Image, Text } from '@chakra-ui/react';
 import Carousel from 'nuka-carousel';
-// import {Link} from 'react-scroll'
+import { useRef } from 'react';
 
 const images = [
     {
@@ -12,7 +12,8 @@ const images = [
 ]
 
 function Slider(){
-
+    const myRef = useRef(null)
+    const executeScroll = () => myRef.current.scrollIntoView({ behavior: 'smooth'})    
     return(
         <Box position='relative'>
             <Carousel withoutControls
@@ -32,13 +33,13 @@ function Slider(){
                     )
                 })}
             </Carousel>
-            {/* <Link> */}
-            <Box display={{base:'none', lg:'block'}}>
+            <Box display={{base:'none', xl:'block'}}>
                 <Button position='absolute' right='100px' bottom='100px'
                         background='cyan' color='red' 
                         height='80px' width='200px'
                         borderRadius='22px'
                         className='kreep'
+                        onClick={executeScroll}
                 >
                     <Box position='absolute' top='0' bottom='0' left='0' right='0' margin='auto' height='25px' >
                         <Text fontSize='xl' fontWeight='bold' textAlign='center'>XEM CỬA HÀNG</Text>
@@ -53,7 +54,7 @@ function Slider(){
                     <ChevronDownIcon w='5em' h='5em' color="cyan.100"/>
                 </Box>
             </Box>
-            {/* </Link> */}
+            <Box ref={myRef}></Box>
         </Box>
     );
 }
