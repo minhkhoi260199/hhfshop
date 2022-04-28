b05442a7082bef9ad28d631e4fc4a272a93d443d
Khôi Nguyễn <minhkhoi260199@gmail.com>
2022-04-28 16:40:33 +0700

    Change baseUrl
    to api service

--- components/helper/config.js
+++ components/helper/config.js
@@ -1,2 +1,2 @@
-export const BASEURL = "http://127.0.0.1:5000";
-// export const BASEURL = "https://hhfservices.herokuapp.com";
+// export const BASEURL = "http://127.0.0.1:5000";
+export const BASEURL = "https://hhfservices.herokuapp.com";
