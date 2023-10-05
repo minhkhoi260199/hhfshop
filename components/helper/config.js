0bcd860ec0c7d0feba7ea78b1b35617f24844915
Khoi Nguyen <46308856+minhkhoi260199@users.noreply.github.com>
2023-10-05 19:15:24 +0700

    Update config.js

--- components/helper/config.js
+++ components/helper/config.js
@@ -1,2 +1,3 @@
 // export const BASEURL = "http://127.0.0.1:5000";
-export const BASEURL = "https://hhfservices.herokuapp.com";
+// export const BASEURL = "https://hhfservices.herokuapp.com"; // old API domain
+export const BASEURL = "https://hiephoafarm1-f6281b53d64a.herokuapp.com/";
